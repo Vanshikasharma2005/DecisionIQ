cbfe619418712d4ea1ac005cca0b8ca73e19158d
Vanshika Sharma <vanshika2005@gmail.com>
2026-08-07 21:17:38 +0530

    Clean frontend Vite boilerplate and setup DecisionIQ base UI

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>DecisionIQ</h1>
+      <p>Smart Product Recommendation System</p>
+    </div>
+  );
+}
+
+export default App;
